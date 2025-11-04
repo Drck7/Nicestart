09a553bab28de2d73ec41665bb27df91cc02bb59
am2dam2 <danielvegaprada@gmail.com>
2025-11-04 14:00:08 +0100

    Acceso SINGUP

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,5 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
 }
--- app/src/main/java/com/example/nicestart/singup.java
+++ app/src/main/java/com/example/nicestart/singup.java
@@ -1,8 +1,6 @@
 package com.example.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class singup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_singup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void openMain(View v){
-        Intent intent = new Intent(Login.this,MainActivity.class);
-        startActivity(intent);
-    }
-    public void openSingup(View v){
-        Intent intent = new Intent(Login.this,singup.class);
-        startActivity(intent);
     }
 }
